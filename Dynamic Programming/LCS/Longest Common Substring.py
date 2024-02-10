36853fd6e7cd6e32143ef106ed00f3dba1aa2ea8
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-10 23:39:58 +0530

    Update Longest Common Substring.py

--- Dynamic Programming/LCS/Longest Common Substring.py
+++ Dynamic Programming/LCS/Longest Common Substring.py
@@ -7,4 +7,8 @@ def func(a,b,m,n):
       if i==0 or j ==0:
         t[i][j]=0
       else:
-        if 
+        if a[m-1] == b[n-1]:
+          t[i][j]=1+t[i-1][j-1]
+          maxi=max(maxi ,t[i][j])
+        else:
+          t[i][j]=0
