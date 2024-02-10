5dfb9189ee1c97fcfe60e603cb8ef968b7d5e5e1
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-10 23:16:25 +0530

    Update LCS DP.py

--- Dynamic Programming/LCS/LCS DP.py
+++ Dynamic Programming/LCS/LCS DP.py
@@ -1,3 +1,12 @@
 def func(a,b,m,n):
-  k
+  t=[[0 for i in range(n+1)]for j in range(m+1)]
+  for i in range(m+1):
+    for j in range(n+1):
+      if i==0 or j==0:
+        t[i][j]=0
+
+  for i in range(1,m+1):
+    for j in range(1,n+1):
+      if(a[i-1]==b[j-1]):
+        t[i][j]=1+
 
