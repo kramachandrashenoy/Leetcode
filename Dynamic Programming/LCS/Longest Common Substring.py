3d8865788281efd434e13673458ef9599a581229
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-10 23:38:21 +0530

    Create Longest Common Substring.py

---
+++ Dynamic Programming/LCS/Longest Common Substring.py
@@ -0,0 +1,10 @@
+def func(a,b,m,n):
+  t=[[0 for i in range(n+1)] for j in range(m+1)]
+  maxi=0
+
+  for i in range(m+1):
+    for j in range(n+1):
+      if i==0 or j ==0:
+        t[i][j]=0
+      else:
+        if 
