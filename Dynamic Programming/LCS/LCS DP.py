45bbc3843e7e3d4d47e9d81012a6266af4e5e743
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-10 23:22:23 +0530

    Update LCS DP.py

--- Dynamic Programming/LCS/LCS DP.py
+++ Dynamic Programming/LCS/LCS DP.py
@@ -8,7 +8,7 @@ def func(a,b,m,n):
   for i in range(1,m+1):
     for j in range(1,n+1):
       if(a[i-1]==b[j-1]):
-        t[i][j]=1+t[i-1[j-1]
+        t[i][j]=1+t[i-1][j-1]
         else:
         t[i][j]=max(t[i-1][j],t[i][j-1])
   return t[m][n]
