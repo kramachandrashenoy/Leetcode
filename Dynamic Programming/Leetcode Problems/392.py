73d1111d33e54cdd50dcea0bf2e82edb130d5255
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-12 00:41:46 +0530

    Update 392.py

--- Dynamic Programming/Leetcode Problems/392.py
+++ Dynamic Programming/Leetcode Problems/392.py
@@ -36,4 +36,21 @@ class Solution(object):
             return True 
         else:
             return False
-        
+
+BETTER CODE:
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i,j=0,0
+        n=len(s)
+        m=len(t)
+        while i<n and j<m:
+            if s[i]==t[j]:
+                i+=1
+            j+=1
+        return i==len(s)
