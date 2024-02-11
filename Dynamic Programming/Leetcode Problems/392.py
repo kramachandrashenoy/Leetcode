d1e1c771d59e709c06468cbc0df5a78fac34dff8
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-12 00:34:03 +0530

    Create 392.py

---
+++ Dynamic Programming/Leetcode Problems/392.py
@@ -0,0 +1,39 @@
+392. Is Subsequence
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+Solution:
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        m=len(t)
+        n=len(s)
+        dp=[[0 for i in range(n+1)]for j in range(m+1)]
+
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if t[i-1]==s[j-1]:
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        if dp[m][n]==n:
+            return True 
+        else:
+            return False
+        
