179123062b5997afa6fa06f3ae86fc61deeae0a2
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-11 08:12:38 +0530

    Update LCS DP.py

--- Dynamic Programming/LCS/LCS DP.py
+++ Dynamic Programming/LCS/LCS DP.py
@@ -1,15 +1,21 @@
-def func(a,b,m,n):
-  t=[[0 for i in range(n+1)]for j in range(m+1)]
-  for i in range(m+1):
-    for j in range(n+1):
-      if i==0 or j==0:
-        t[i][j]=0
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        m=len(text1)
+        n=len(text2)
+        t=[[0 for i in range(n+1)]for j in range(m+1)]
 
-  for i in range(1,m+1):
-    for j in range(1,n+1):
-      if(a[i-1]==b[j-1]):
-        t[i][j]=1+t[i-1][j-1]
-        else:
-        t[i][j]=max(t[i-1][j],t[i][j-1])
-  return t[m][n]
-
+        for i in range(m+1):
+            for j in range(n+1):
+                if i==0 or j==0:
+                    t[i][j]=0
+                else:
+                    if text1[i-1]==text2[j-1]:
+                        t[i][j]=1+t[i-1][j-1]
+                    else:
+                        t[i][j]=max(t[i-1][j],t[i][j-1])
+        return t[m][n]
