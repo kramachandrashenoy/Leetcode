7eef1461686caa287bc87745415e549528b6636b
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-12 12:38:15 +0530

    Update Minimum Number of Insertion and Deletion to convert String a to String b.py

--- Dynamic Programming/LCS/Minimum Number of Insertion and Deletion to convert String a to String b.py
+++ Dynamic Programming/LCS/Minimum Number of Insertion and Deletion to convert String a to String b.py
@@ -1,1 +1,23 @@
+class Solution:
+	def minOperations(self, s1, s2):
+	    lcs = LCSDP(s1, s2, len(s1), len(s2))
+	    numOfDel = len(s1) - lcs
+	    numOfIns = len(s2) - lcs
+	    return numOfDel + numOfIns
 
+
+def LCSDP(X, Y, n, m):
+    dp = [[-1 for i in range(m+1)] for j in range(n+1)]
+    for i in range(n+1):
+        for j in range(m+1):
+            if i==0 or j==0:
+                dp[i][j] = 0
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if(X[i-1] == Y[j-1]):
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    
+    return dp[n][m]
