a75e3beb8009b629d2113e815e13e292c4a7aeaf
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-02-21 21:17:02 +0530

    Update Largest Subarray of sum K.py

--- Arrays and Strings/Sliding Window/Largest Subarray of sum K.py
+++ Arrays and Strings/Sliding Window/Largest Subarray of sum K.py
@@ -1,5 +1,5 @@
 Given an array containing N positive integers and an integer K. Your task is to find the length of the longest Sub-Array with sum of the elements equal to the given value K.
-
+Only for positive input values.
 For Input:
 4 1 1 1 2 3 5
 5
