e1237f4596d792bfe4f644fafd514f9c5eb5dd40
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-03-08 21:11:22 +0530

    Update N Meeting.py

--- Greedy/N Meeting.py
+++ Greedy/N Meeting.py
@@ -1,4 +1,4 @@
-https://www.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
+'''https://www.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
 There is one meeting room in a firm. There are N meetings in the form of (start[i], end[i]) where start[i] is start time of meeting i and end[i] is finish time of meeting i.
 What is the maximum number of meetings that can be accommodated in the meeting room when only one meeting can be held in the meeting room at a particular time?
 
@@ -14,7 +14,7 @@ Output:
 Explanation:
 Maximum four meetings can be held with
 given start and end timings.
-The meetings are - (1, 2),(3, 4), (5,7) and (8,9)
+The meetings are - (1, 2),(3, 4), (5,7) and (8,9)'''
 
 Solution:
 #Function to find the maximum number of meetings that can
