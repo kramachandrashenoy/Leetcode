e2d5a4096b74d920a3da8d01a8417c47a574fbb3
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-06-10 19:18:45 +0530

    Create 1051.py

---
+++ Greedy/Leetcode problems/1051.py
@@ -0,0 +1,14 @@
+1051. Height Checker
+
+Solution:
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected=sorted(heights)
+        count=0
+        for i in range(len(heights)):
+            if heights[i]!=expected[i]:
+                count+=1
+        return count
+            
+        
