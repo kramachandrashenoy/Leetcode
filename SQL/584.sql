ac3502f2d9ee6b841aaac338caaa2bc468611a4d
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-07-04 21:26:34 +0530

    Create 584.sql

---
+++ SQL/584.sql
@@ -0,0 +1,6 @@
+https://leetcode.com/problems/find-customer-referee/description/?envType=study-plan-v2&envId=top-sql-50
+
+Solution:
+
+# Write your MySQL query statement below
+select name from Customer where referee_id!=2 or referee_id is null;
