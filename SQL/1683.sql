da129efa64d251ba3a4048d042e89b5b7f5ec8b6
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-07-08 14:42:19 +0530

    Create 1683.sql

---
+++ SQL/1683.sql
@@ -0,0 +1,7 @@
+https://leetcode.com/problems/invalid-tweets/description/?envType=study-plan-v2&envId=top-sql-50
+
+Solution:
+
+# Write your MySQL query statement below
+select tweet_id from Tweets
+where LENGTH(content)>15;
